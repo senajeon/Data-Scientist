cd53538b6d328f7a0b03faf8cbd6cd41fbfeab7e
Sen <senajeon0921@gmail.com>
2020-07-24 11:49:02 +0900

    Update 1. create tables.sql

--- MYSQL/1. create tables.sql
+++ MYSQL/1. create tables.sql
@@ -1,11 +1,9 @@
 -- Creat tables/primary key/foreign key/unique key 
 
-DROP table sales;
-
 CREATE DATABASE IF NOT EXISTS Sales;
 CREATE TABLE sales
 (
-	purchase_number INT NOT NULL AUTO_INCREMENT,
+    purchase_number INT NOT NULL AUTO_INCREMENT,
     date_of_purchase DATE NOT NULL,
     customer_id INT,
     item_code VARCHAR(10) NOT NULL,
